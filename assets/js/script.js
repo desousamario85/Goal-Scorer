42465a10f84078f64513a5885d74de4325fd6bf5
desousamario85 <desousa.mario@yahoo.com>
2022-02-22 00:56:45 +0000

    Start game function added. created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("I'm Connected")
+let startDiv = document.getElementById("start-game");
+let endDiv = document.getElementById("end-game")
+
+console.log(startDiv)
+startDiv.addEventListener('click', startGame)
+
+function startGame() {
+
+    let goalSetting = 13;
+
+    for (let i = 1; i < goalSetting; i++) {
+        let goalsNet = document.getElementById("goals-posts-outer")
+        let element = document.createElement('class')
+        element.className = "goals-posts-inner"
+        goalsNet.appendChild(element)
+        goalsNet.children.item(i - 1).innerHTML = i
+
+        console.log(`Goal ${i} net create`)
+
+
+    }
+    startDiv.style.visibility = "hidden"
+    endDiv.style.visibility = "visible"
+
+}
